72e8c5c03dc816780c1af5c7c746c12e234c01e2
kraj2503 <73212876+kraj2503@users.noreply.github.com>
2025-05-23 00:21:52 +0530

    Removed unwanted scripts and added processor,config and scripts

---
+++ apps/processor/index.ts
@@ -0,0 +1,40 @@
+import { PrismaClient } from "@prisma/client";
+import { Kafka } from "kafkajs";
+import { TOPIC_NAME } from "@repo/config";
+
+
+const client = new PrismaClient();
+const kafka = new Kafka({
+  clientId: 'outBoxProcessor',
+  brokers: ['localhost:9092']
+})
+
+
+async function main() {
+const producer = kafka.producer();
+await producer.connect()
+
+  while (1) {
+    const pendingRows = await client.taskRunOutbox.findMany({
+      where: {},
+      take: 10,
+    });
+
+
+    await producer.sendBatch({
+    topicMessages: [
+      {
+        topic: TOPIC_NAME,
+        messages: pendingRows.map(row => ({
+            value: row.taskRunId,
+          })),
+      }
+    ],
+  });
+
+
+
+  }
+}
+
+main();
