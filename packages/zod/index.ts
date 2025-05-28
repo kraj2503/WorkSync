fecd4462727eb2c856673a4dd5aecad908d019f6
kraj2503 <73212876+kraj2503@users.noreply.github.com>
2025-05-29 01:47:26 +0530

    Init Frontend

--- packages/zod/index.ts
+++ packages/zod/index.ts
@@ -1,4 +1,5 @@
 import z from "zod";
+import type { email } from "zod/v4";
 
 export const SignUpSchema = z.object({
   userName: z.string().min(5),
@@ -6,6 +7,19 @@ export const SignUpSchema = z.object({
   email: z.string().email(),
 });
 export const SignInSchema = z.object({
-  userName: z.string(),
+ email: z.string().email(),
   password: z.string(),
+});
+
+export const TaskCreateSchema = z.object({
+  trigger: z.object({
+    availableTriggerId: z.string(),
+    triggerMetadata: z.any().optional()
+  }),
+  actions: z.array(
+    z.object({
+      availableActionId: z.string(),
+      actionMetadata:z.any().optional()
+    })
+  ),
 });
