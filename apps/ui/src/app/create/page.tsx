f19564958918a5316843a37bd63986aa1d30a0a7
kraj2503 <73212876+kraj2503@users.noreply.github.com>
2025-07-10 00:55:47 +0530

    taskCell

--- apps/ui/src/app/create/page.tsx
+++ apps/ui/src/app/create/page.tsx
@@ -1,15 +1,20 @@
+"use client"
+import { useState } from "react";
 import Appbar from "../components/Appbar";
+import { TaskCell } from "../components/taskCell";
 
 
 export default function () {
-    
+     
+    const [selectedTrigger, setSelectedTrigger] = useState("")
+    const [selectedAction ,setSelectedAction] = useState([])
 
     return (
         <>
-            <Appbar />
-            <div className="">
+            {/* <Appbar /> */}
+            <div className=" flex justify-center">
 
-                this is create new page
+         <TaskCell name = {selectedTrigger?selectedTrigger:"Trigger"} index = {1}/>
 
 
             </div>
