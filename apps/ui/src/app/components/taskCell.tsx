95a932719f21dbd87e08114e57835da8fbb48cb3
kraj2503 <73212876+kraj2503@users.noreply.github.com>
2025-07-10 23:42:52 +0530

    Create page-add task

--- apps/ui/src/app/components/taskCell.tsx
+++ apps/ui/src/app/components/taskCell.tsx
@@ -1,7 +1,7 @@
 export const TaskCell = ({ name, index }: { name?: string; index: number }) => {
   return (
           <div className="flex">
-      <div className="border border-black py-5 px-4 flex w-80 justify-center bg-red-200">
+      <div className="border border-black py-5 px-4 flex w-[300px] justify-center cursor-pointer">
               
       <div className="font-bold">{index}</div>
 
--- apps/ui/src/app/create/page.tsx
+++ apps/ui/src/app/create/page.tsx
@@ -1,24 +1,51 @@
-"use client"
+"use client";
 import { useState } from "react";
 import Appbar from "../components/Appbar";
 import { TaskCell } from "../components/taskCell";
+import { Button } from "@/components/ui/button";
 
+export default function TaskFlow() {
+  const [selectedTrigger, setSelectedTrigger] = useState("");
+  const [selectedAction, setSelectedAction] = useState<
+    {
+      availableActionId: string;
+      availableActionName: string;
+    }[]
+  >([]);
 
-export default function () {
-     
-    const [selectedTrigger, setSelectedTrigger] = useState("")
-    const [selectedAction ,setSelectedAction] = useState([])
-
-    return (
-        <>
-            {/* <Appbar /> */}
-            <div className=" flex justify-center">
-
-         <TaskCell name = {selectedTrigger?selectedTrigger:"Trigger"} index = {1}/>
-
-
-            </div>
-        </>
-    )
+  return (
+    <>
+      {/* <Appbar /> */}
+      <div className="flex justify-center flex-col min-h-screen">
+        <div className="flex justify-center w-full">
+          <TaskCell name={selectedTrigger || "Trigger"} index={1} />
+        </div>
+       
+          {selectedAction.map((action, index) => (
+           <div className="flex justify-center w-full mt-5">
+              <TaskCell
+              key={action.availableActionId}
+              name={action.availableActionName || "Action"}
+              index={2 + index}
+              />
+              </div>
+          ))}
+        <Button variant={"link"}
+          onClick={() => {
+            setSelectedAction((a) => [
+              ...a,
+              {
+                availableActionId: "",
+                availableActionName: "",
+              },
+            ]);
+          }}
+              ><div className="text-2xl">
+                      
+          +
+        </div>
+        </Button>
+      </div>
+    </>
+  );
 }
-    
