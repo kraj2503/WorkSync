49f801acbe89d62a5b34b61d0935ba34003dbbe9
kraj2503 <73212876+kraj2503@users.noreply.github.com>
2025-07-14 01:51:10 +0530

    TaskCell adv

--- apps/ui/src/app/components/taskCell.tsx
+++ apps/ui/src/app/components/taskCell.tsx
@@ -1,12 +1,19 @@
-export const TaskCell = ({ name, index }: { name?: string; index: number }) => {
+export const TaskCell = ({
+  name,
+  index,
+  onClick,
+}: {
+  name?: string;
+  index: number;
+  onClick: () => void;
+}) => {
   return (
-          <div className="flex">
+    <div className="flex" onClick={onClick}>
       <div className="border border-black py-5 px-4 flex w-[300px] justify-center cursor-pointer">
-              
-      <div className="font-bold">{index}</div>
+        <div className="font-bold">{index}</div>
 
-      {name}
+        {name}
+      </div>
     </div>
-          </div>
   );
 };
